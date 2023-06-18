9a61a689a7d69fd23cc4c95161ea4e6e9ef0adf0
Bilalzafar0020 <bilalafridi0020@gmail.com>
2023-06-19 00:48:59 +0500

    My first express server

--- main.mjs
+++ main.mjs
@@ -1,27 +1,66 @@
-console.log('Hello world');
+// console.log('Hello world');
 
+
+// import express from 'express';
+
+// const app = express()
+// const port = 3001
+
+// app.get('/', (req, res) => {
+//     res.set('Content-Type', 'text/html');
+//     res.status(200).send("<h1>Hello world Server</h1>");
+// })
+
+// app.get('/profile', (req, res)=>{
+//     res.set('Content-Type', 'text/html');
+//     res.status(200).send("<h1>Hello World server</h1>");v
+// } )
+
+
+
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
 
 import express from 'express';
+import open from 'open';
 
-const app = express()
-const port = 3001
+const app = express();
+const port = 3002;
 
 app.get('/', (req, res) => {
     res.set('Content-Type', 'text/html');
-    res.status(200).send("<h1>Hello world Server</h1>");
-})
+    res.status(200).send(`
+        <html>
+            <body>
+                <h1>Hello World Server</h1>
+                <button onclick="openBrowser()">Open Browser</button>
 
-app.get('/profile', (req, res)=>{
+                <script>
+                    function openBrowser() {
+                        window.open('http://localhost:${port}/profile');
+                    }
+                </script>
+            </body>
+        </html>
+    `);
+});
+
+app.get('/profile', (req, res) => {
     res.set('Content-Type', 'text/html');
-    res.status(200).send("<h1>Hello World server</h1>");v
-} )
-
-
-
+    res.status(200).send(`
+        <html>
+            <body>
+                <h1>Hello World Server</h1>
+            </body>
+        </html>
+    `);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
+    console.log(`Server is running on port ${port}`);
+    open(`http://localhost:${port}`);
+});
 
 
